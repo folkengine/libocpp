a0d1634afccda0d4c9fc528d27e184628d183625
Coury Richards <146002925+couryrr-afs@users.noreply.github.com>
2024-05-07 13:59:31 +0000

    Updated tests file format and handled TODOs
    Signed-off-by: Coury Richards <146002925+couryrr-afs@users.noreply.github.com>

--- tests/lib/ocpp/v201/test_database_handler.cpp
+++ tests/lib/ocpp/v201/test_database_handler.cpp
@@ -11,11 +11,12 @@ class DatabaseHandlerV201 : public testing::Test {
 
 public:
     DatabaseHandlerV201() {
-        std::unique_ptr<common::DatabaseConnection> dbc = std::make_unique<common::DatabaseConnection>("file::memory:?cache=shared");
+        std::unique_ptr<common::DatabaseConnection> dbc =
+            std::make_unique<common::DatabaseConnection>("file::memory:?cache=shared");
         dbc->open_connection(); // Open connection so memory stays shared
 
-        this->db_handler = std::make_unique<DatabaseHandler>(std::move(dbc),
-                                                             std::filesystem::path(MIGRATION_FILES_LOCATION_V201));
+        this->db_handler =
+            std::make_unique<DatabaseHandler>(std::move(dbc), std::filesystem::path(MIGRATION_FILES_LOCATION_V201));
         this->db_handler->open_connection();
         this->db_connection = std::make_unique<common::DatabaseConnection>("file::memory:?cache=shared");
         db_connection->open_connection(); // Open connection so memory stays shared
@@ -29,46 +30,36 @@ protected:
 };
 
 TEST_F(DatabaseHandlerV201, KO1_FR27_DatabaseWithNoData_InsertProfile) {
-    db_handler->insert_or_update_charging_profile(1, ChargingProfile{1,1});
+    db_handler->insert_or_update_charging_profile(1, ChargingProfile{1, 1});
     std::string sql = "SELECT COUNT(*) FROM CHARGING_PROFILES";
     auto select_stmt = db_connection->new_statement(sql);
-    
-    if (select_stmt->step() != SQLITE_ROW) {
-        // TODO ...
-        return;
-    }
 
+    ASSERT_TRUE(select_stmt->step() == SQLITE_ROW);
     auto count = select_stmt->column_int(0);
     ASSERT_EQ(count, 1);
 }
 
 TEST_F(DatabaseHandlerV201, KO1_FR27_DatabaseWithProfileData_UpdateProfile) {
-    db_handler->insert_or_update_charging_profile(1, ChargingProfile{2,1});
-    db_handler->insert_or_update_charging_profile(1, ChargingProfile{2,2});
+    db_handler->insert_or_update_charging_profile(1, ChargingProfile{2, 1});
+    db_handler->insert_or_update_charging_profile(1, ChargingProfile{2, 2});
 
     std::string sql = "SELECT COUNT(*) FROM CHARGING_PROFILES";
     auto select_stmt = db_connection->new_statement(sql);
-    
-    if (select_stmt->step() != SQLITE_ROW) {
-        // TODO ...
-        return;
-    }
+
+    ASSERT_TRUE(select_stmt->step() == SQLITE_ROW);
 
     auto count = select_stmt->column_int(0);
     ASSERT_EQ(count, 1);
 }
 
 TEST_F(DatabaseHandlerV201, KO1_FR27_DatabaseWithProfileData_InsertNewProfile) {
-    db_handler->insert_or_update_charging_profile(1, ChargingProfile{1,1});
-    db_handler->insert_or_update_charging_profile(1, ChargingProfile{2,1});
+    db_handler->insert_or_update_charging_profile(1, ChargingProfile{1, 1});
+    db_handler->insert_or_update_charging_profile(1, ChargingProfile{2, 1});
 
     std::string sql = "SELECT COUNT(*) FROM CHARGING_PROFILES";
     auto select_stmt = db_connection->new_statement(sql);
-    
-    if (select_stmt->step() != SQLITE_ROW) {
-        // TODO ...
-        return;
-    }
+
+    ASSERT_TRUE(select_stmt->step() == SQLITE_ROW);
 
     auto count = select_stmt->column_int(0);
     ASSERT_EQ(count, 2);
